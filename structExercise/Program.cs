6ea35a79d6a13a9d7d6ac49cc91394d074b41003
Michael Kever <michaelkever@gmail.com>
2018-07-22 14:03:28 -0500

    StructExercise

---
+++ structExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structExercise
+{
+    class Program
+    {
+        public struct Number
+        {
+            public decimal Amount;
+        }
+          
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 1;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+         
+
+    }
+}
